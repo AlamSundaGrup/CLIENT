77a1e463c95a7b92b154c0e94ba94f253f1594de
ptindotechteknologi <email@ptindotechteknologi.com>
2024-10-10 00:13:28 +0700

    feat make fitur ractcontext untuk profile

---
+++ client_side/src/contexts/ProfileContext.jsx
@@ -0,0 +1,56 @@
+import React, { createContext, useContext, useState, useEffect } from "react";
+import axios from "axios";
+
+const ProfileContext = createContext();
+
+export const ProfileProvider = ({ children }) => {
+  const [profiles, setProfiles] = useState([]);
+  const [loading, setLoading] = useState(true);
+  const [error, setError] = useState(null);
+
+  const fetchProfiles = async () => {
+    try {
+      const token = localStorage.getItem("access_token");
+      const response = await axios.get("http://localhost:3000/profiles", {
+        headers: {
+          Authorization: `Bearer ${token}`,
+        },
+      });
+      setProfiles(response.data);
+      setLoading(false);
+    } catch (err) {
+      console.log(err);
+    }
+  };
+
+  const fetchProfileById = async (id) => {
+    try {
+      const token = localStorage.getItem("access_token");
+      const response = await axios.get(`http://localhost:3000/profiles/${id}`, {
+        headers: {
+          Authorization: `Bearer ${token}`,
+        },
+      });
+      return response.data;
+    } catch (err) {
+      console.error(err);
+      return null;
+    }
+  };
+
+  useEffect(() => {
+    fetchProfiles();
+  }, [fetchProfiles]);
+
+  return (
+    <ProfileContext.Provider
+      value={{ profiles, loading, error, fetchProfileById }}
+    >
+      {children}
+    </ProfileContext.Provider>
+  );
+};
+
+export const useProfile = () => {
+  return useContext(ProfileContext);
+};
